530a1a20b8cbcbf63a93c855edf1ed54e1d482cb
Furkan Emin <femincanx@gmail.com>
2022-09-23 21:24:17 +0300

    feat: add custom theme

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,36 @@
 import { createTheme } from '@mui/material';
 
-export default createTheme({});
+export default createTheme({
+  palette: {
+    common: {
+      black: '#000814',
+      white: '#F2F2F2',
+    },
+    redRYB: {
+      main: '#FF2625',
+      contrastText: '#F2F2F2',
+    },
+    redPigment: {
+      main: '#E62121',
+      contrastText: '#F2F2F2',
+    },
+    jet: {
+      main: '#303030',
+      contrastText: '#F2F2F2',
+    },
+    richBlack: {
+      main: '#000814',
+      contrastText: '#F2F2F2',
+    },
+    cultured: {
+      main: '#F2F2F2',
+      contrastText: '000814',
+    },
+    text: {
+      primary: '#000814',
+    },
+  },
+  typography: {
+    logoFontFamily: 'Ubuntu, sans-serif',
+  },
+});
