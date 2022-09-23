395dd7d337daefc25555f8b427039ce0b2bbacbe
Furkan Emin <femincanx@gmail.com>
2022-09-23 21:27:17 +0300

    refactor: delete unnecessary lines

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Container } from '@mui/material';
 
-import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
