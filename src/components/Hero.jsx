238ce76627ae63d924c2b8745ff5addad037f11e
Furkan Emin <femincanx@gmail.com>
2022-09-23 22:49:44 +0300

    feat: add Hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,76 @@
+import { Box, Button, Stack, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+import BannerImage from '../assets/images/banner.png';
+
+const Hero = () => {
+  return (
+    <Stack
+      direction="row"
+      justifyContent="space-around"
+      alignItems="center"
+      color="common.white"
+      minHeight="100vh"
+    >
+      <Stack
+        alignSelf="stretch"
+        justifyContent="space-around"
+        alignItems="center"
+        position="relative"
+      >
+        <Stack
+          justifyContent="center"
+          flex={3}
+          gap={3}
+          textAlign={{ sm: 'center', md: 'start' }}
+        >
+          <Typography
+            variant="h4"
+            component="p"
+            color="cultured.main"
+            fontWeight={300}
+          >
+            Fitness Club
+          </Typography>
+          <Typography variant="h2" component="p">
+            Sweat, Smile <br /> and Repeat
+          </Typography>
+          <Typography
+            variant="h6"
+            component="p"
+            color="cultured.main"
+            fontWeight={300}
+          >
+            Check out most effective exercises
+          </Typography>
+        </Stack>
+        <Box sx={{ width: 1, flex: 1, textAlign: 'center' }}>
+          <Button
+            component={Link}
+            to="/exercises"
+            variant="contained"
+            color="redPigment"
+            sx={{
+              height: 1 / 3,
+              width: 8 / 10,
+              fontSize: 18,
+              fontWeight: 400,
+              letterSpacing: 1.5,
+            }}
+          >
+            Explore Exercises
+          </Button>
+        </Box>
+      </Stack>
+      <Box
+        component="img"
+        src={BannerImage}
+        sx={{
+          display: { sm: 'none', md: 'block' },
+          width: 2 / 5,
+        }}
+      ></Box>
+    </Stack>
+  );
+};
+
+export default Hero;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import { Box, Container } from '@mui/material';
+
+import Hero from '../components/Hero';
+import SearchExercise from '../components/SearchExercise';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Box sx={{ bgcolor: 'black.main' }}>
+      <Container sx={{ minHeight: '100vh' }}>
+        <Hero />
+        <SearchExercise />
+      </Container>
+    </Box>
+  );
 };
 
 export default Home;
