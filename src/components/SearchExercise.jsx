67f550b4be25e4d96b0b24383e061ccb85304f17
Furkan Emin <femincanx@gmail.com>
2022-10-01 09:00:44 +0300

    feat: make component responsive

--- src/components/SearchExercise.jsx
+++ src/components/SearchExercise.jsx
@@ -1,7 +1,6 @@
 import {
   Button,
   Checkbox,
-  FormControl,
   InputBase,
   MenuItem,
   Select,
@@ -26,64 +25,67 @@ const SearchExercise = ({ selectedBodyParts, setSelectedBodyParts }) => {
 
   return (
     <Stack
-      direction="row"
+      direction={{ xs: 'column', sm: 'row' }}
       justifyContent="center"
       alignItems="center"
       spacing={2}
       py={3}
     >
-      <FormControl sx={{ width: 150 }}>
-        <Select
-          multiple
-          value={selectedBodyParts}
-          onChange={handleSelectedBodyParts}
-          input={<InputBase />}
-          renderValue={(selected) => (
-            <Typography variant="body2">
-              Fitler by Body Part <br />
-              <Typography
-                variant="caption"
-                component="span"
-                color="redPigment.main"
-                sx={{
-                  display: selected.includes('all') ? 'none' : 'inline',
-                }}
-              >
-                {selected.length} Selected
-              </Typography>
-            </Typography>
-          )}
-        >
-          {bodyParts.map((bodyPart) => (
-            <MenuItem
-              key={bodyPart}
-              value={bodyPart}
+      <Select
+        multiple
+        value={selectedBodyParts}
+        onChange={handleSelectedBodyParts}
+        input={<InputBase />}
+        renderValue={(selected) => (
+          <Typography variant="body2">
+            Fitler by Body Part <br />
+            <Typography
+              variant="caption"
+              component="span"
               color="redPigment.main"
               sx={{
-                '&.Mui-selected': { bgcolor: 'rgba(230, 33, 33, 0.1)' },
-                '&.Mui-selected:hover': { bgcolor: 'rgba(230, 33, 33, 0.2)' },
+                display: selected.includes('all') ? 'none' : 'inline',
               }}
             >
-              <Checkbox
-                checked={selectedBodyParts.includes(bodyPart)}
-                color="redPigment"
-              />
-              {bodyPart}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+              {selected.length} Selected
+            </Typography>
+          </Typography>
+        )}
+        sx={{ width: 150, height: 30 }}
+      >
+        {bodyParts.map((bodyPart) => (
+          <MenuItem
+            key={bodyPart}
+            value={bodyPart}
+            sx={{
+              '&.Mui-selected': { bgcolor: 'rgba(230, 33, 33, 0.1)' },
+              '&.Mui-selected:hover': { bgcolor: 'rgba(230, 33, 33, 0.2)' },
+            }}
+          >
+            <Checkbox
+              checked={selectedBodyParts.includes(bodyPart)}
+              color="redPigment"
+            />
+            {bodyPart}
+          </MenuItem>
+        ))}
+      </Select>
       <TextField
         color="redPigment"
         label="Search"
         placeholder="Search exercise, target muscle or equipment"
-        sx={{ width: 4 / 10 }}
+        sx={{ width: { xs: 1, sm: 4 / 10 } }}
       />
       <Button
         variant="contained"
         color="redPigment"
         size="large"
-        sx={{ height: 56, width: 120, padding: 1.5, letterSpacing: 0.9 }}
+        sx={{
+          height: { xs: 48, sm: 56 },
+          width: { xs: 140, sm: 120 },
+          padding: 1.5,
+          letterSpacing: 0.9,
+        }}
       >
         Search
       </Button>
